0f1ae138fb93241bfee3ae5f5ddf220e66b48156
Jahaziel Rodriguez <68198368+Young-Prota@users.noreply.github.com>
2021-03-10 19:15:14 -0400

    Transitions
    Added smooth transitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      'dev-gray': '#252629',
+      'dev-purple': '#953DBA',
+      'dev-green': '#D2E566'
+  },
   },
   variants: {
     extend: {},
