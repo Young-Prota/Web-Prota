84980107ffac5447b925b86bdb03a519d072eb1f
Jahaziel Rodriguez <68198368+Young-Prota@users.noreply.github.com>
2021-03-11 02:04:44 -0400

    Darkmode reference

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     colors: {
       'dev-gray': '#252629',
       'dev-purple': '#953DBA',
-      'dev-green': '#D2E566'
+      'dev-green': '#D2E566',
+      'dev-white': '#F8F8F8'
+
   },
   },
   variants: {
